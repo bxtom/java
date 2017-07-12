04fb6ac95f65dd61eba54a3215f0c67a4a70ece5
Tomek <java-tomek@users.noreply.github.com>
2017-07-12 10:38:41 +0200

    zadanie domowe 03

---
+++ zaj-3-4-zadania-domowe/03/CarType.java
@@ -0,0 +1,7 @@
+/*
+* 3. (Poziom 1) Zaprojektuj typ enum TypAuta, składający się z nazw typów samochodów: OSOBOWY, CIĘŻAROWY, SPORTOWY.
+* */
+
+public enum CarType {
+    PASSENGER_CAR, TRUCK, SPORTS_CAR
+}
