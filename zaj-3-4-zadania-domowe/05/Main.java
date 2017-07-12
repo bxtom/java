e54de7635284fba351f44388582a55e08e02ce85
Tomek <java-tomek@users.noreply.github.com>
2017-07-12 20:53:01 +0200

    zadanie domowe 05

--- zaj-3-4-zadania-domowe/05/Main.java
+++ zaj-3-4-zadania-domowe/05/Main.java
@@ -10,8 +10,10 @@ public class Main {
     public static void main(String[] args) {
         Building building1 = new Building(30, 50, 20);
         System.out.println(building1.floorArea());
+
         StoreyBuilding building2 = new StoreyBuilding(30, 50, 40);
         System.out.println(building2.floorArea());
+
         Skyscraper building3 = new Skyscraper(30, 50, 100, 5);
         System.out.println(building3.floorArea());
     }
