bfc277d7695ef15a04834dcf331993acab9ab2cb
Tomek <java-tomek@users.noreply.github.com>
2017-07-13 11:56:34 +0200

    test usuwania z githuba

--- zaj-3-4-zadania-domowe/14/src/main/java/Calculator.java
+++
@@ -1,9 +0,0 @@
-public class Calculator {
-    public static int addition(int a, int b) {
-        return a + b;
-    }
-
-    public static int subtraction(int a, int b) {
-        return a - b;
-    }
-}
